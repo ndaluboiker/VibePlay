15f8d408676c4522b3b166f68362c15c14def477
ndaluboiker <ndaluboiker3@gmail.com>
2026-04-05 21:24:13 +0300

    Add migrate command to build script

--- build.sh
+++ build.sh
@@ -2,7 +2,11 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
 
+# Convert static files
 python manage.py collectstatic --no-input
+
+# Apply any outstanding database changes (THIS IS THE KEY)
 python manage.py migrate
